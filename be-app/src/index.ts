2ab1bf47ba0c6d1a2bfe219f96b4c9977202a714
vietthom <thomle0418@gmail.com>
2023-12-15 00:13:33 -0500

    Connect backend to frontend

--- be-app/src/index.ts
+++ be-app/src/index.ts
@@ -1,6 +1,13 @@
 import express, {Request, Response} from 'express';
+import cors from 'cors';
 
 const app = express()
+
+const configOptions = {
+    origin: 'http://localhost:3000'
+}
+
+app.use(cors(configOptions))
 
 app.get('/api/v1/ping', (req: Request, res: Response) => {
     res.send({
--- fe-app/src/App.tsx
+++ fe-app/src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
 
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/ping')
+        const result = await response.json()
+
+        setData(result.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <p>{data}</p>
+    </main>
   )
 }
 
